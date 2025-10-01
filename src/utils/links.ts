b719795585c8720fdc3a7a2bff5a661081353c2f
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-01 16:00:50 -0500

    Add RRSS utility: create links.ts file with social media URLs

---
+++ src/utils/links.ts
@@ -0,0 +1,6 @@
+export const RRSS = {
+  instagram: "https://www.instagram.com/tuberias_peruanito_s.a.c/",
+  whatsapp:
+    "https://api.whatsapp.com/send/?phone=51922723633&text=Hola+Tuber%C3%ADas+Peruanito%2C+vengo+de+la+p%C3%A1gina+web%2C+tengo+una+consulta+&type=phone_number&app_absent=0",
+  tiktok: "https://www.tiktok.com/@tuberias_peruanito_s.a.c",
+};
