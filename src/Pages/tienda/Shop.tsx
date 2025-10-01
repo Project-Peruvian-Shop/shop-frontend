55a9a8fa00f73dd55506b4c38c14ce931838ca51
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-01 17:17:08 -0500

    Update Sidebar styles: adjust padding and divisor width

--- src/Pages/tienda/Shop.tsx
+++ src/Pages/tienda/Shop.tsx
@@ -1,15 +1,19 @@
+import Products from "../../Components/shop/products/Products";
 import Sidebar from "../../Components/shop/sidebar/Sidebar";
 import SubHeader from "../../Components/shop/subheader/SubHeader";
 import { mockSidebar } from "../../utils/mock";
+import styles from "./Shop.module.css";
 
 const Shop = () => {
   return (
     <>
       <SubHeader />
 
-      <Sidebar arrayCategories={mockSidebar.arrayCategories} />
+      <div className={styles.shopContainer}>
+        <Sidebar arrayCategories={mockSidebar.arrayCategories} />
 
-      <div>Productos</div>
+        <Products />
+      </div>
     </>
   );
 };
