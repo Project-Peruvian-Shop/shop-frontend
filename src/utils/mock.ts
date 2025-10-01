e6fcdf60605d256d063f384ef30d0dbd84fa55be
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-01 17:11:52 -0500

    Add mock data for Sidebar component categories

---
+++ src/utils/mock.ts
@@ -0,0 +1,9 @@
+import type { SidebarProps } from "../Components/shop/sidebar/Sidebar";
+
+export const mockSidebar: SidebarProps = {
+  arrayCategories: [
+    { id: 1, name: "Categoría 1", quantity: 10 },
+    { id: 2, name: "Categoría 2", quantity: 5 },
+    { id: 3, name: "Categoría 3", quantity: 8 },
+  ],
+};
