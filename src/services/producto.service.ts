c39a14a51346e0aa5e89e9faff90f780ddae7804
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-02 11:41:41 -0500

    Add missing import for ProductoDashboardDTO in producto.service.ts

--- src/services/producto.service.ts
+++ src/services/producto.service.ts
@@ -3,6 +3,7 @@ import { URL_API } from "../utils/constants";
 import type { ApiResponse, PaginatedResponse } from "./global.interfaces";
 import type {
   PaginatedProductoResponseDTO,
+  ProductoDashboardDTO,
   ProductoDTO,
 } from "../models/Producto/Producto_response_dto";
 
@@ -25,6 +26,17 @@ export async function getProductoById(id: number): Promise<ProductoDTO> {
   const url = `${BASE_URL}/${id}`;
 
   const res = await axios.get<ApiResponse<ProductoDTO>>(url);
+
+  return res.data.data;
+}
+
+export async function getAllProductos(
+  page: number = 0,
+  size: number = 10
+): Promise<ProductoDashboardDTO[]> {
+  const url = `${BASE_URL}/dashboard-paginated?page=${page}&size=${size}`;
+
+  const res = await axios.get<ApiResponse<ProductoDashboardDTO[]>>(url);
 
   return res.data.data;
 }
