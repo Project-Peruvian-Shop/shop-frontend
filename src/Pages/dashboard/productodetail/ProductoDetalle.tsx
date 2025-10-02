eb383a048b5dfd4bf8cd28548bcf3c8113b7990b
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-02 16:29:47 -0500

    Refactor ProductoDetalle component to enhance product display and layout

--- src/Pages/dashboard/productodetail/ProductoDetalle.tsx
+++ src/Pages/dashboard/productodetail/ProductoDetalle.tsx
@@ -1,6 +1,34 @@
+import { useNavigate, useParams } from "react-router-dom";
+import type { ProductoDTO } from "../../../models/Producto/Producto_response_dto";
 import styles from "./ProductoDetalle.module.css";
+import { useEffect, useState } from "react";
+import { getProductoById } from "../../../services/producto.service";
+import { routes } from "../../../utils/routes";
 
 function ProductoDetalle() {
+  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+
+  const [producto, setProducto] = useState<ProductoDTO | null>(null);
+
+  useEffect(() => {
+    if (!id) {
+      navigate(routes.shop);
+      return;
+    }
+
+    const fetchProducto = async (id: string) => {
+      try {
+        const data = await getProductoById(Number(id));
+        setProducto(data);
+      } catch (error) {
+        console.error("Error al obtener el producto:", error);
+        navigate(routes.profile_user);
+      }
+    };
+
+    fetchProducto(id);
+  }, [id, navigate]);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -15,11 +43,49 @@ function ProductoDetalle() {
 
       <div className={styles.content}>
         <div className={styles.left}>
-          <div className={styles.datos}></div>
-          <div className={styles.detalles}></div>
+          <div className={styles.card}>
+            <div className={styles.subtitle}>Datos del Producto</div>
+            <div className={styles.infoRow}>
+              <span className={styles.label}>ID:</span>
+              <span className={styles.value}>{producto?.id}</span>
+            </div>
+            <div className={styles.infoRow}>
+              <span className={styles.label}>Nombre:</span>
+              <span className={styles.value}>{producto?.nombre}</span>
+            </div>
+            <div className={styles.infoRow}>
+              <span className={styles.label}>Categoría:</span>
+              <span className={styles.value}>{producto?.categoriaNombre}</span>
+            </div>
+            <div className={styles.infoRow}>
+              <span className={styles.label}>Descripción:</span>
+              <span className={styles.value}>
+                {producto?.descripcion}
+                duradera.
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <div className={styles.subtitle}>Detalles técnicos</div>
+
+            <div className={styles.infoRow}>
+              <span className={styles.label}>Norma:</span>
+              <span className={styles.value}>{producto?.categoriaNombre}</span>
+            </div>
+
+            <div className={styles.infoRow}>
+              <span className={styles.label}>Usos:</span>
+              <span className={styles.value}>{producto?.categoriaUsos}</span>
+            </div>
+          </div>
         </div>
         <div className={styles.right}>
-          <img src="" alt="Producto" className={styles.productImage} />
+          <img
+            src={producto?.productoEnlace}
+            alt={producto?.productoAlt}
+            className={styles.productImage}
+          />
         </div>
       </div>
     </div>
