5bd05828667e13497fe132f3c89592419fb837a1
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-02 14:58:33 -0500

    Implement Dashboard layout with responsive design and structured components

--- src/Pages/dashboard/principal/Dashboard.tsx
+++ src/Pages/dashboard/principal/Dashboard.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import styles from "./Dashboard.module.css";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.graphic}>
+          <div className={styles.title}>Total de cotizaciones por mes</div>
+          <div className={styles.chart}></div>
+        </div>
+
+        <div className={styles.messageProducts}>
+          <div className={styles.messages}>
+            <div className={styles.title}>Mensajes Pendientes</div>
+          </div>
+
+          <div className={styles.productos}>
+            <div className={styles.title}>Productos más cotizados del mes</div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.right}>
+        <div className={styles.topClients}>
+          <div className={styles.title}>Últimas cotizaciones</div>
+        </div>
+
+        <div className={styles.topSellers}>
+          <div className={styles.title}>Categorías más cotizadas</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
