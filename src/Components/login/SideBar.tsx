4017cd9ddf9d2b9209fd43c29d655e16376e0f77
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-02 05:19:55 -0500

    Add SideBar component with styles for navigation and display

---
+++ src/Components/login/SideBar.tsx
@@ -0,0 +1,33 @@
+import styles from "./SideBar.module.css";
+import logo from "../../Icons/Logo-HD.png";
+import Icon from "../../Icons/star.svg";
+
+interface SideBarProps {
+  title: string;
+  subtitle: string;
+  list: string[];
+}
+
+function SideBar(props: SideBarProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo-tuberias-peruanito" className={styles.logo} />{" "}
+      </div>
+      <div className={styles.title}>{props.title}</div>
+
+      <div className={styles.subtitle}>{props.subtitle}</div>
+
+      <ul className={styles.list}>
+        {props.list.map((item, index) => (
+          <li key={index} className={styles.listItem}>
+            <img src={Icon} alt="icon" className={styles.bulletIcon} />
+            <span>{item}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SideBar;
