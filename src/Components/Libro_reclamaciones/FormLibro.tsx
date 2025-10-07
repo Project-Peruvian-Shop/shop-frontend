1fc65834cb3d075f45dafecf25bb531d115dda1e
joaoDevL <joao.software.dev@gmail.com>
2025-10-06 22:58:23 -0500

    Adding styles to the complaint book

---
+++ src/Components/Libro_reclamaciones/FormLibro.tsx
@@ -0,0 +1,114 @@
+import style from "./FormLibro.module.css";
+
+const FormLibro = () => {
+
+  return (
+    <div className={style.containerFormulario}>
+      <h3>Libro de Reclamaciones</h3>
+      <form >
+          <h3>Datos personales</h3>
+        <div className={style.inputGroup}>
+          <div className={style.inputWrapper}>
+            <label htmlFor="fullName">Nombre Completo / Razón Social*</label>
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              placeholder="Ingrese su nombre completo o de la empresa"
+              required
+            />
+          </div>
+          <div className={style.inputWrapper}>
+            <label htmlFor="documentType">Tipo de documento*</label>
+            <select
+              id="documentType"
+              name="documentType"
+              required
+            >
+              <option value="">Seleccione</option>
+              <option value="dni">DNI</option>
+              <option value="ruc">RUC</option>
+            </select>
+          </div>
+            <div className={style.inputWrapper}>
+            <label htmlFor="documentNumber">Número de documento*</label>
+            <input
+              type="number"
+              id="documentNumber"
+              name="documentNumber"
+              placeholder="Ingrese su número de documento"
+              required
+            />
+          </div>
+          <div className={style.inputWrapper}>
+            <label htmlFor="phoneNumber">Teléfono / Celular*</label>
+            <input
+              type="number"
+              id="phoneNumber"
+              name="phoneNumber"
+              placeholder="Ingrese su número de teléfono o celular"
+              required
+            />
+          </div>
+        </div>
+        <div className={style.radioGroup}>
+          <h3 className={style.radioGroupTitle}>Tipo de solicitud</h3>
+          <div className={style.radioContainer}>
+            <label className={style.radioLabel}>
+              <div className={style.radioCard}>
+                <input
+                  type="radio"
+                  name="tipoReclamo"
+                  value="reclamo"
+                    className={style.radioInput}
+                  required
+                />
+                <span className={style.radioTitle}>Reclamo</span>
+                <span className={style.radioDescription}>
+                  Disconformidad relacionada a los productos
+                </span>
+              </div>
+            </label>
+            <label className={style.radioLabel}>
+              <div className={style.radioCard}>
+                <input
+                  type="radio"
+                  name="tipoReclamo"
+                  value="queja"
+                  className={style.radioInput}
+                  required
+                />
+                <span className={style.radioTitle}>Queja</span>
+                <span className={style.radioDescription}>
+                  Disconformidad no relacionada a los productos
+                </span>
+              </div>
+            </label>
+          </div>
+        </div>
+      <div className={style.textareaGroup}>
+        <div className={style.inputWrapper}>
+          <label htmlFor="message">Detalle de la reclamación</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Describa de manera clara y detallada los hechos que motivan su reclamo o queja..."
+            required
+            />
+        </div>
+        </div>
+        <div className={style.termsBox}>
+            <input type="checkbox" id="terms" required />
+            <label htmlFor="terms">
+              Confirmo que la información proporcionada es verídica.
+            </label>
+            <button type="submit" className={style.btnPrimary}>
+              Enviar reclamación
+            </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FormLibro;
---
+++ src/Pages/Libro_reclamaciones/Complaints_book.tsx
@@ -0,0 +1,39 @@
+import Banner from "../../Components/banner/Banner";
+import logoTiktok from '../../Icons/tik-tok.png';
+import logoInstagram from '../../Icons/instagram.png';
+import logoWhatsApp from '../../Icons/whatsapp.png';
+import style from './Complaints_book.module.css';
+import FormLibro from "../../Components/Libro_reclamaciones/FormLibro";
+const Complaints_book = () => {
+    return (
+    <main>
+        <Banner title="Libro de Reclamaciones"/>
+        <div className={style.intro}>
+            <p>
+            Aquí puedes registrar una queja, presentar un reclamo o enviar sugerencias fácilmente. 
+            Tu opinión es importante para nosotros y estamos comprometidos en mejorar continuamente nuestro servicio. 
+            Por favor, proporciona toda la información necesaria para que podamos atender tu solicitud de manera rápida y efectiva.
+            </p>
+        </div>
+        <div className={style.containerContacto}>
+            <div className={style.containerRedes}>
+                <p>Télefono/Whatsapp</p>
+                <p>+51 922 723 633</p>
+            </div>
+            <div className={style.containerRedes}>
+                <p>Email</p>
+                <p>wilmer.guevara@tuberiasperuanito.com</p>
+            </div>
+            <div className={style.containerRedes}>
+                <p>Redes Sociales</p>
+                <span><a href="http://" target="_blank" rel="noopener noreferrer"><img src={logoWhatsApp} alt="whatsapp" /></a></span>
+                <span><a href="http://" target="_blank" rel="noopener noreferrer"><img src={logoInstagram} alt="instagram" /></a></span>
+                <span><a href="http://" target="_blank" rel="noopener noreferrer"><img src={logoTiktok} alt="tiktok" /></a></span>
+            </div>
+        </div>
+        <FormLibro />
+    </main>
+    )
+}
+
+export default Complaints_book;
