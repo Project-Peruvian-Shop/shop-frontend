14381c878aa023dac9688371147893ed2112879d
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-08 11:03:59 -0500

    feat: add user data loading in FormContactenos component

--- src/Components/Contactenos/FormContactenos.tsx
+++ src/Components/Contactenos/FormContactenos.tsx
@@ -4,7 +4,7 @@ import { routes } from "../../utils/routes";
 import { obtenerUsuario } from "../../utils/auth";
 import withReactContent from "sweetalert2-react-content";
 import Swal from "sweetalert2";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContactenos } from "../../services/mensajes.service";
 
 const FormContactenos = () => {
@@ -20,6 +20,33 @@ const FormContactenos = () => {
   const [telefono, setTelefono] = useState("");
   const [email, setEmail] = useState("");
   const [contenido, setContenido] = useState("");
+
+  useEffect(() => {
+    if (usuario) {
+      // Cargar los valores del usuario en el formulario
+      setNombre(usuario.nombre || "");
+      setEmail(usuario.email || "");
+      setTipoDocumento(usuario.tipoDocumento || "");
+      setDocumento(usuario.documento || "");
+      setTelefono(usuario.telefono || "");
+
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Se han cargado tus datos personales",
+      });
+    }
+  }, [usuario]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
