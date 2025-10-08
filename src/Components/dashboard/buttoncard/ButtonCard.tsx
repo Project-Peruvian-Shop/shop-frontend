eecc125bc14d52376904acef94ff5412500cf280
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-08 09:54:03 -0500

    feat: enhance ButtonCard component with variant support and style adjustments

--- src/Components/dashboard/buttoncard/ButtonCard.tsx
+++ src/Components/dashboard/buttoncard/ButtonCard.tsx
@@ -1,25 +1,43 @@
+import { COLORS } from "../../../utils/constants";
 import styles from "./ButtonCard.module.css";
+
+export type VariantType = "primary" | "secondary" | "logout";
 
 interface ButtonCardProps {
   icon: string;
   text: string;
+  variant?: VariantType;
   onClick: () => void;
-  bgColor?: string;
 }
 
 export default function ButtonCard({
   icon,
   text,
+  variant = "primary",
   onClick,
-  bgColor,
 }: ButtonCardProps) {
+  const style = {
+    primary: {
+      borderColor: COLORS.primary,
+      backgroundColor: COLORS.primary,
+      color: COLORS.white,
+    },
+    secondary: {
+      borderColor: COLORS.secondary,
+      backgroundColor: COLORS.white,
+      color: COLORS.secondary,
+    },
+    logout: {
+      borderColor: COLORS.grayBtn,
+      backgroundColor: COLORS.grayBtn,
+      color: COLORS.white,
+    },
+  };
+
   return (
-    <button
-      onClick={onClick}
-      className={styles.card}
-      style={bgColor ? { backgroundColor: bgColor } : {}}
-    >
+    <button onClick={onClick} className={styles.card} style={style[variant]}>
       <img src={icon} alt={text} className={styles.icon} />
+
       <span className={styles.text}>{text}</span>
     </button>
   );
