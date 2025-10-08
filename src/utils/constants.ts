57edf9bbeeb5a7e922689b9285f38d7906bd2c69
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-08 09:54:07 -0500

    feat: add COLORS constant for color palette management

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,12 @@
 export const URL_API = "http://localhost:8080/api/v1";
+
+export const COLORS = {
+  primary: "#fb2343",
+  primaryDark: "#fb234378",
+  secondary: "#1b2f33",
+  secondaryDark: "#1b2f3378",
+  gray: "#c7c7c728",
+  white: "#ffffff",
+  black: "#000000",
+  grayBtn: "#848484",
+};
