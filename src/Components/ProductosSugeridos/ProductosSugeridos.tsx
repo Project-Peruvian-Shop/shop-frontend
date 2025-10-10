92f77d3366e573447a3acb9841268693d5754851
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-10 11:46:32 -0500

    refactor: simplify rendering logic in ProductosSugeridos component for better readability

--- src/Components/ProductosSugeridos/ProductosSugeridos.tsx
+++ src/Components/ProductosSugeridos/ProductosSugeridos.tsx
@@ -22,26 +22,20 @@ function ProductosSugeridos(props: ProductosSugeridosProps) {
   );
 
   useEffect(() => {
-    if (!producto || !categoria) {
-      navigate(routes.shop);
-      return;
-    }
-
     const fetchProducto = async () => {
       try {
         setLoading(true);
         const response = await getSugeridos(producto, categoria);
         setProductos(response);
       } catch (err) {
-        console.error("Error cargando producto:", err);
-        navigate(routes.shop);
+        console.error("Error cargando productos sugeridos: ", err);
       } finally {
         setLoading(false);
       }
     };
 
     fetchProducto();
-  }, [producto, categoria, navigate]);
+  }, [producto, categoria]);
 
   const handleAddToCart = async (producto: PaginatedProductoResponseDTO) => {
     const redirect = await addToCart(producto);
@@ -52,29 +46,27 @@ function ProductosSugeridos(props: ProductosSugeridosProps) {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.usosContainer}>
-        <div className={styles.subtitle}>Productos sugeridos:</div>
-        {loading ? (
-          <div>Cargando...</div>
-        ) : productos.length === 0 ? (
-          <div>No hay productos sugeridos disponibles.</div>
-        ) : (
-          <div className={styles.productosGrid}>
-            {productos.map((producto) => (
-              <ProductCard
-                key={producto.id}
-                link={`${routes.product}${producto.id}`}
-                img={producto.imagenUrl}
-                title={producto.nombre}
-                alt={producto.imagenAlt}
-                click={() => handleAddToCart(producto)}
-              />
-            ))}
+    <>
+      {!loading && productos?.length > 0 && (
+        <div className={styles.container}>
+          <div className={styles.usosContainer}>
+            <div className={styles.subtitle}>Productos sugeridos:</div>
+            <div className={styles.productosGrid}>
+              {productos.map((sugerido) => (
+                <ProductCard
+                  key={sugerido.id}
+                  link={`${routes.product}${sugerido.id}`}
+                  img={sugerido.imagenUrl}
+                  title={sugerido.nombre}
+                  alt={sugerido.imagenAlt}
+                  click={() => handleAddToCart(sugerido)}
+                />
+              ))}
+            </div>
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
 
