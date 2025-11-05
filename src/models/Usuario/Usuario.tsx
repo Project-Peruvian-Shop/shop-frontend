6501444b1fe9ac0f02844105bf423e9fb2b976d8
tineojf <92743762+tineodev@users.noreply.github.com>
2025-11-05 17:36:12 -0500

    refactor: Update Usuario interface to use UserRole type and add role constants

--- src/models/Usuario/Usuario.tsx
+++ src/models/Usuario/Usuario.tsx
@@ -1,9 +1,23 @@
 export interface Usuario {
-    id: number;
-    nombre: string;
-    apellidos: string;
-    email: string;
-    passwordd: string;
-    telefono: string;
-    rol: "ROLE_ADMIN" | "ROLE_USER" | "ROLE_MANAGER"
+  id: number;
+  nombre: string;
+  apellidos: string;
+  email: string;
+  passwordd: string;
+  telefono: string;
+  rol: UserRole;
 }
+
+// añadir roles
+export type UserRole =
+  | "ROLE_SUPERADMIN"
+  | "ROLE_ADMINISTRADOR"
+  | "ROLE_SUPERVISOR"
+  | "ROLE_CLIENTE";
+
+export const UserRoleConst = {
+  SUPERADMIN: "ROLE_SUPERADMIN" as UserRole,
+  ADMINISTRADOR: "ROLE_ADMINISTRADOR" as UserRole,
+  SUPERVISOR: "ROLE_SUPERVISOR" as UserRole,
+  CLIENTE: "ROLE_CLIENTE" as UserRole,
+};
